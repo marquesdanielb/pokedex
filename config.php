2ff02e5e56d7ca6042549d7a517f0c6febd972e5
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2022-08-19 12:22:00 -0300

    Posicionando e estilizando os elementos

--- config.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-$mysql = mysqli_connect('localhost', 'pokedexadmin', 'root', 'pokedex');
-$mysql->set_charset('utf8');
-
-if (!$mysql) {
-    echo 'banco não conectado';
-}
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$url = "https://www.canalti.com.br/api/pokemons.json";
-$pokemonJson = file_get_contents($url);
-$pokemons = json_decode($pokemonJson, false);
